cc928ed9eaa73e3281d900041ff61ca5925be489
Tomer Ron <tomer@tomerron.com>
2021-02-18 22:06:13 +0200

    test(e2e): increase timeout between e2e tests to prevent ci from failing

--- e2e/setupTests.ts
+++ e2e/setupTests.ts
@@ -1,8 +1,8 @@
 /**
- * Wait 1 second before each test.
+ * Wait 2 seconds before each test.
  * This is required to prevent flakiness.
  * Otherwise, the client might fail to login.
  */
 beforeEach(async () => {
-  await new Promise<void>((resolve) => setTimeout(resolve, 1000))
+  await new Promise<void>((resolve) => setTimeout(resolve, 2000))
 })
